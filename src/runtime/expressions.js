323162f4a93af992c48c768c99f76b19fbeafde1
potatodesigner <36785132+potatodesigner@users.noreply.github.com>
2019-01-15 18:50:00 +0800

    1
    1

--- src/runtime/expressions.js
+++ src/runtime/expressions.js
@@ -7,17 +7,17 @@ var ls;
 	};
 	ls.MainScene = function() {
 		return {
-			"Math.random()*2%2B3": function() { return Math.random()*2+3 },
-			"fen.mark": function() { return fen.mark },
-			"null": function() { return null },
+			"%22tween%22": function() { return "tween" },
 			"%22quadOut%22": function() { return "quadOut" },
-			"%22GAMEOVER%22": function() { return "GAMEOVER" },
+			"null": function() { return null },
+			"Math.random()*2%2B3": function() { return Math.random()*2+3 },
+			"%22mark%22": function() { return "mark" },
 			"%22MainScene%22": function() { return "MainScene" },
 			"AISprite1570": function() { return AISprite1570 },
 			"%22equalTo%22": function() { return "equalTo" },
-			"%22mark%22": function() { return "mark" },
-			"%22tween%22": function() { return "tween" },
-			"%22quadIn%22": function() { return "quadIn" }
+			"%22quadIn%22": function() { return "quadIn" },
+			"fen.mark": function() { return fen.mark },
+			"%22GAMEOVER%22": function() { return "GAMEOVER" }
 		}
 	};
 })(ls || (ls = {}));
